a1ebaae675ff52a1b68a684e48516d7ea212da16
krrishdholakia <krrishdholakia@gmail.com>
2018-07-28 22:13:10 -0400

    minor fixes

--- frontend/components/MainContent.js
+++ frontend/components/MainContent.js
@@ -15,13 +15,14 @@ class MainContent extends React.Component {
             errorEmail:"",
             errorQuestion:"",
             showSuccess: false,
-            showFailure: false
-
+            showFailure: false, 
+            showFAQ1: false 
         }       
         
         this.handleEmail = this.handleEmail.bind(this)
         this.handleQuestion = this.handleQuestion.bind(this)
         this.onSubmit = this.onSubmit.bind(this)
+        this.handleFAQ = this.handleFAQ.bind(this)
         console.log("errorEmail: ", this.state.errorEmail)
     }
 
@@ -31,6 +32,12 @@ class MainContent extends React.Component {
 
     handleQuestion(e) {
         this.setState({response: e.target.value})
+    }
+
+    handleFAQ(e) {
+        if (e == 1) {
+            this.setState({showFAQ1: !this.state.showFAQ1})
+        } 
     }
 
     onSubmit() {
@@ -65,34 +72,48 @@ class MainContent extends React.Component {
     render() {
         const {errorEmail, errorQuestion} = this.state
         return (
-            <div className="main-content"> 
-                <h3 className = "main-title"> Traditional Crowdfunding made smart. </h3>
-                <p className = "main-description">
-                    Delphi is an attempt to use the convenience and safety of smart-contracts provided by the Ethereum blockchain network
-                    in traditional crowdfunding platforms like Kickstarter and Indiegogo. 
-                </p>
-                <h3 className = "sign-up"> Sign Up for the Beta! </h3>
-                <div className="sign-up-email-container">
-                    <input type="email" className="sign-up-email" placeholder="Email Address.." onChange={this.handleEmail}/>
-                    <div className="error">{this.state.errorEmail? this.state.errorEmail:null}</div>
+            <div className="main-content-container">
+                <div className="main-content"> 
+                    <h3 className = "main-title"> Traditional Crowdfunding made smart. </h3>
+                    <p className = "main-description">
+                        Delphi uses the convenience and safety of smart-contracts provided by the Ethereum blockchain network
+                        in traditional crowdfunding platforms like Kickstarter and Indiegogo. 
+                    </p>
+                    <div>
+                        <h3 onClick={() => this.handleFAQ(1)}> What are smart contracts ? </h3> 
+                        <h3 style={{display: this.state.showFAQ1 ? 'block' : 'none'}}> Contracts which are built atop a blockchain. </h3>
+                        <h3 onClick={() => this.handleFAQ(2)}> What are smart contracts ? </h3> 
+                        <h3 style={{display: this.state.showFAQ1 ? 'block' : 'none'}}> Contracts which are built atop a blockchain. </h3>
+                        <h3 onClick={() => this.handleFAQ(3)}> What are smart contracts ? </h3> 
+                        <h3 style={{display: this.state.showFAQ1 ? 'block' : 'none'}}> Contracts which are built atop a blockchain. </h3>
+                        <h3 onClick={() => this.handleFAQ(4)}> What are smart contracts ? </h3> 
+                        <h3 style={{display: this.state.showFAQ1 ? 'block' : 'none'}}> Contracts which are built atop a blockchain. </h3>
+                        <h3 onClick={() => this.handleFAQ(5)}> What are smart contracts ? </h3> 
+                        <h3 style={{display: this.state.showFAQ1 ? 'block' : 'none'}}> Contracts which are built atop a blockchain. </h3> 
                     </div>
-                <div className="sign-up-question-container">
-                    <textarea className="sign-up-question" placeholder="What kind of contract would you like with your patron/campaign creator?.." onChange={this.handleQuestion}/>
-                    <div className="error">{this.state.errorQuestion? this.state.errorQuestion:null}</div>
-                    <h3 className="sign-up-submit" onClick={this.onSubmit}> Sign Up! </h3>
+                    <h3 className = "sign-up"> Sign Up for the Beta! </h3>
+                    <div className="sign-up-email-container">
+                        <input type="email" className="sign-up-email" placeholder="Email Address.." onChange={this.handleEmail}/>
+                        <div className="error">{this.state.errorEmail? this.state.errorEmail:null}</div>
+                        </div>
+                    <div className="sign-up-question-container">
+                        <textarea className="sign-up-question" placeholder="What kind of contract would you like with your patron/campaign creator?.." onChange={this.handleQuestion}/>
+                        <div className="error">{this.state.errorQuestion? this.state.errorQuestion:null}</div>
+                        <h3 className="sign-up-submit" onClick={this.onSubmit}> Sign Up! </h3>
+                    </div>
+                    <SweetAlert
+                        success
+                        show={this.state.showSuccess}
+                        title="You're all signed up!"
+                        onConfirm={() => this.setState({ showSuccess: false })}
+                        />
+                    <SweetAlert
+                        error
+                        show={this.state.showFailure}
+                        title="Error signing up... Try again!"
+                        onConfirm={() => this.setState({ showFailure: false })}
+                        />
                 </div>
-                <SweetAlert
-                    success
-                    show={this.state.showSuccess}
-                    title="You're all signed up!"
-                    onConfirm={() => this.setState({ showSuccess: false })}
-                    />
-                <SweetAlert
-                    error
-                    show={this.state.showFailure}
-                    title="Error signing up... Try again!"
-                    onConfirm={() => this.setState({ showFailure: false })}
-                    />
             </div>
         )
     }
